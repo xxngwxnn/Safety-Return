6025ef8de6b9caedf4fc51d6b2b45fd3fc343c53
aqwo6333 <130898021+aqwo6333@users.noreply.github.com>
2023-12-17 17:20:58 +0900

    Update LoginActivity.java

--- app/src/main/java/com/example/SafetyReturn/LoginActivity.java
+++ app/src/main/java/com/example/SafetyReturn/LoginActivity.java
@@ -14,26 +14,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // 선언 및 초기화
     private EditText editUsername, editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // XML 레이아웃 파일과 연결
         setContentView(R.layout.activity_login);
 
+        // XML에서 정의한 EditText들을 코드와 연결
         editUsername = findViewById(R.id.editUsername);
         editPassword = findViewById(R.id.editPassword);
 
+        // 버튼과의 연결
         Button loginButton = findViewById(R.id.loginButton);
         Button idFindButton = findViewById(R.id.idfindButton);
         Button pwFindButton = findViewById(R.id.pwfindButton);
 
+        // 로그인 버튼 클릭 이벤트
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 로그인 메소드 호출
                 login();
             }
         });
+
+        // 아이디 찾기 화면으로 이동하는 버튼 클릭 이벤트
         idFindButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +50,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // 비밀번호 찾기 화면으로 이동하는 버튼 클릭 이벤트
         pwFindButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,22 +60,32 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    // 로그인 메소드
     private void login() {
+        // 입력된 아이디와 비밀번호 가져오기
         String inputUsername = editUsername.getText().toString();
         String inputPassword = editPassword.getText().toString();
+
+        // SharedPreferences를 통해 저장된 아이디와 비밀번호 가져오기
         SharedPreferences preferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
         String savedUsername = preferences.getString("username", "");
         String savedPassword = preferences.getString("password", "");
         SharedPreferences.Editor editor = preferences.edit();
 
+        // 입력된 아이디와 비밀번호가 저장된 정보와 일치하는지 확인
         if (inputUsername.equals(savedUsername) && inputPassword.equals(savedPassword)) {
-                        showSuccessDialog();
+            // 로그인 성공 시 성공 다이얼로그 표시
+            showSuccessDialog();
+            // 로그인 상태를 저장하여 앱 재시작 시 로그인 상태를 유지
             editor.putBoolean("isLoggedIn", true);
             editor.apply();
         } else {
+            // 로그인 실패 시 실패 다이얼로그 표시
             showFailureDialog();
         }
     }
+
+    // 로그인 성공 다이얼로그 표시 메소드
     private void showSuccessDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("로그인 성공");
@@ -74,11 +93,14 @@ public class LoginActivity extends AppCompatActivity {
                 .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        // 메인 액티비티로 이동
                         navigateToMainActivity();
                     }
                 })
                 .show();
     }
+
+    // 로그인 실패 다이얼로그 표시 메소드
     private void showFailureDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("로그인 실패");
@@ -86,9 +108,12 @@ public class LoginActivity extends AppCompatActivity {
                 .setNegativeButton("확인", null)
                 .show();
     }
+
+    // 메인 액티비티로 이동하는 메소드
     private void navigateToMainActivity() {
         Intent mainIntent = new Intent(this, MainActivity.class);
         startActivity(mainIntent);
+        // 현재 액티비티 종료
         finish();
     }
 }
