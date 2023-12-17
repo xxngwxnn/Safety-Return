b9de744068efcf69ebd6f3dab4e330c9277180ce
aqwo6333 <130898021+aqwo6333@users.noreply.github.com>
2023-12-17 17:24:34 +0900

    Update TimerActivity.java

--- app/src/main/java/com/example/SafetyReturn/TimerActivity.java
+++ app/src/main/java/com/example/SafetyReturn/TimerActivity.java
@@ -1,76 +1,126 @@
 package com.example.SafetyReturn;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TimerExpiredActivity extends AppCompatActivity {
+public class TimerActivity extends AppCompatActivity {
 
-    private TextView timerExpiredTextView;
+    private EditText timerHourEditText;
+    private EditText timerMinuteEditText;
+    private EditText timerSecondEditText;
+    private Button startTimerButton;
+    private Button stopTimerButton;
+    private TextView timerTextView;
     private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timer_expired);
+        setContentView(R.layout.activity_timer);
 
-        // XML 레이아웃에서 TextView와 Button 위젯 초기화
-        timerExpiredTextView = findViewById(R.id.timerExpiredTextView);
-        Button cancelButton = findViewById(R.id.cancelButton);
+        // XML 레이아웃에서 각 위젯 초기화
+        timerHourEditText = findViewById(R.id.timerHourEditText);
+        timerMinuteEditText = findViewById(R.id.timerMinuteEditText);
+        timerSecondEditText = findViewById(R.id.timerSecondEditText);
+        startTimerButton = findViewById(R.id.startTimerButton);
+        stopTimerButton = findViewById(R.id.stopTimerButton);
+        timerTextView = findViewById(R.id.timerTextView);
 
-        // 취소 버튼에 OnClickListener 설정
-        cancelButton.setOnClickListener(new View.OnClickListener() {
+        // 시작 버튼 클릭 이벤트 리스너 설정
+        startTimerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 취소 버튼 클릭 시 MainActivity로 이동하고 현재 액티비티 종료
-                Intent intent = new Intent(TimerExpiredActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                finish();
+                // 타이머 시작 메소드 호출
+                startTimer();
             }
         });
 
-        // 카운트다운 타이머 시작
-        startCountdownTimer(1 * 60 * 1000);
+        // 중지 버튼 클릭 이벤트 리스너 설정
+        stopTimerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 타이머 중지 메소드 호출
+                stopTimer();
+            }
+        });
     }
 
-    // 카운트다운 타이머를 시작하는 메소드
-    private void startCountdownTimer(long millisInFuture) {
-        countDownTimer = new CountDownTimer(millisInFuture, 1000) {
+    // 타이머 시작 메소드
+    private void startTimer() {
+        // 입력된 시간 값 가져오기
+        String hourText = timerHourEditText.getText().toString().trim();
+        String minuteText = timerMinuteEditText.getText().toString().trim();
+        String secondText = timerSecondEditText.getText().toString().trim();
+
+        // 빈 값 처리
+        if (hourText.isEmpty()) {
+            hourText = "0";
+        }
+
+        if (minuteText.isEmpty()) {
+            minuteText = "0";
+        }
+
+        if (secondText.isEmpty()) {
+            secondText = "0";
+        }
+
+        // 시간, 분, 초 값으로 변환
+        int hours = Integer.parseInt(hourText);
+        int minutes = Integer.parseInt(minuteText);
+        int seconds = Integer.parseInt(secondText);
+
+        // 총 밀리초 계산
+        long totalTimeInMillis = ((hours * 60 + minutes) * 60 + seconds) * 1000;
+
+        // CountDownTimer 설정
+        countDownTimer = new CountDownTimer(totalTimeInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                // 1초마다 호출되며 남은 시간을 텍스트뷰에 업데이트
+                // 남은 시간을 텍스트뷰에 업데이트
                 updateTimerText(millisUntilFinished);
             }
 
             @Override
             public void onFinish() {
-                // 타이머 종료 시 메시지 발송 안내 메시지로 텍스트 설정
-                timerExpiredTextView.setText("        메시지가 발송되었습니다.");
+                // 타이머 종료 후 TimerExpiredActivity로 이동
+                updateTimerText(0);
+                showTimerExpiredActivity();
             }
         }.start();
     }
 
-    // 텍스트뷰에 표시될 시간을 업데이트하는 메소드
-    private void updateTimerText(long millisUntilFinished) {
-        int seconds = (int) (millisUntilFinished / 1000) % 60;
-        // 포맷에 맞추어 텍스트 설정
-        String timerText = String.format(" -- %02d 초", seconds);
-        timerExpiredTextView.setText("                타이머가 종료되었습니다.\n              1분이내에 취소 하지 않으면, \n등록된 비상연락망으로 메시지가 발송됩니다.\n                             " + timerText);
+    // TimerExpiredActivity로 이동하는 메소드
+    private void showTimerExpiredActivity() {
+        Intent intent = new Intent(this, TimerExpiredActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // 액티비티가 종료될 때 카운트다운 타이머가 실행 중이면 취소
+    // 타이머 중지 메소드
+    private void stopTimer() {
+        // CountDownTimer가 존재하면 중지하고 텍스트 업데이트
         if (countDownTimer != null) {
             countDownTimer.cancel();
+            updateTimerText(0);
         }
+    }
+
+    // 타이머 텍스트 업데이트 메소드
+    private void updateTimerText(long millisUntilFinished) {
+        // 남은 시간을 시, 분, 초로 변환하여 텍스트뷰에 표시
+        int hours = (int) (millisUntilFinished / 1000) / 3600;
+        int minutes = ((int) (millisUntilFinished / 1000) % 3600) / 60;
+        int seconds = (int) (millisUntilFinished / 1000) % 60;
+
+        String timerText = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        timerTextView.setText("남은 시간: " + timerText);
     }
 }
